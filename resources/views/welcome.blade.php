ce2be892a2562abec5bcd72c91770181908de2d2
black-code1 <legrandmahamat1@gmail.com>
2022-06-08 14:50:50 +0100

    Prerequisite - Blade Components Explained

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-section>
+        Hello There
+    </x-section>
+
+    <x-section>
+        Hello There Again
+    </x-section>
+
+    <x-section>
+        Hello There Three Times
+    </x-section>
+</x-layout>
