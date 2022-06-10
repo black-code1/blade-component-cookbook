b20d4d7abcb3c89d1aa4daeb1f233dff387eee65
black-code1 <legrandmahamat1@gmail.com>
2022-06-10 09:47:14 +0100

    Dropdown Component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,18 @@
 <x-layout>
-    <x-modal title="Deactivate Your Account?" submit-label="Deactivate">
+    <x-section>
+        <div>
+            <x-dropdown>
 
-        <x-slot name="trigger">
-            <button @click="on = true">Show Modal</button>
-        </x-slot>
+                <x-slot name="trigger">
+                    <button>Click Me</button>
+                </x-slot>
 
-        Are you sure really you want to deactivate your account? All of your data will be permanently removed. This
-        action cannot be undone.
-    </x-modal>
+                <x-dropdown-link href="/">One</x-dropdown-link>
+                <x-dropdown-link href="/">Two</x-dropdown-link>
+                <x-dropdown-link href="/">Three</x-dropdown-link>
+
+            </x-dropdown>
+        </div>
+
+    </x-section>
 </x-layout>
